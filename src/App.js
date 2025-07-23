7825059bc9522200b61d4102dd9b53cf687eba1a
Mileva <mileva3010@gmail.com>
2025-07-23 12:13:42 +0200

    initial commit 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <h1>Hello World</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
