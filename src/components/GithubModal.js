5c486228cb8533d91c764e221cc3b2f42da82133
Mileva <mileva3010@gmail.com>
2025-07-24 19:54:56 +0200

    add modal

---
+++ src/components/GithubModal.js
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+import {
+  FaCodeBranch,
+  FaInfoCircle,
+  FaMapMarkerAlt,
+  FaUser,
+  FaUsers,
+} from "react-icons/fa";
+import "./GithubModal.css";
+
+function GithubModal() {
+  const [githubData, setGithubData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/github-john-doe")
+      .then((res) => res.json())
+      .then((data) => setGithubData(data))
+      .catch((error) => console.error("Erreur de chargement :", error));
+  }, []);
+
+  return (
+    <div
+      className="modal fade"
+      id="githubModal"
+      tabIndex="-1"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-dialog-centered modal-lg">
+        <div className="modal-content custom-modal-content">
+          {/* Ajout du header avec le titre et bouton fermeture */}
+          <div className="modal-header">
+            <h4 className="modal-title">Mon profil GitHub</h4>
+            <button
+              type="button"
+              className="btn-close btn-close-white"
+              data-bs-dismiss="modal"
+              aria-label="Fermer"
+            ></button>
+          </div>
+
+          <div className="modal-body">
+            {githubData ? (
+              <div className="modal-grid">
+                <div className="left-column">
+                  <img
+                    src={githubData.avatar_url}
+                    alt={githubData.name}
+                    className="avatar"
+                  />
+                </div>
+                <div className="right-column">
+                  <p>
+                    <FaUser className="icon" />{" "}
+                    <a
+                      href={githubData.html_url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="github-link"
+                    >
+                      {githubData.name}
+                    </a>
+                  </p>
+                  <hr />
+                  <p>
+                    <FaMapMarkerAlt className="icon" />{" "}
+                    {githubData.location || "2, Rue de Paris 94000 Créteil"}
+                  </p>
+                  <hr />
+                  <p>
+                    <FaInfoCircle className="icon" /> {githubData.bio}
+                  </p>
+                  <hr />
+                  <p>
+                    <FaCodeBranch className="icon" /> Repos publics :{""}
+                    {githubData.public_repos}
+                  </p>
+                  <hr />
+                  <p>
+                    <FaUsers className="icon" /> Followers :{" "}
+                    {githubData.followers}
+                  </p>
+                  <hr />
+                  <p>
+                    <FaUsers className="icon" /> Following :{" "}
+                    {githubData.following}
+                  </p>
+                </div>
+              </div>
+            ) : (
+              <p className="text-light">Chargement...</p>
+            )}
+          </div>
+
+          <div className="modal-footer border-0 justify-content-end">
+            <button
+              type="button"
+              className="btn btn-light"
+              data-bs-dismiss="modal"
+            >
+              Fermer
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GithubModal;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,94 +1,41 @@
+import React, { useState, useEffect } from "react";
 import "./Home.css";
+import GithubModal from "../components/GithubModal";
 
 function Home() {
+  const [githubData, setGithubData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/octocat") // Remplace par ton username GitHub
+      .then((res) => res.json())
+      .then((data) => setGithubData(data))
+      .catch((err) => console.error("Erreur GitHub :", err));
+  }, []);
+
   return (
     <div className="home">
       <div className="home-container-p1">
-        <div className="img-hero"> </div>
+        <div className="img-hero"></div>
         <div className="content">
           <h1 className="HomeTitle">Bonjour, je suis John Doe</h1>
           <h2 className="HomeSubtitle1">Développeur web full stack</h2>
           <div>
-            <button className="btn btn-danger">En savoir plus</button>
+            <button
+              className="btn btn-danger"
+              data-bs-toggle="modal"
+              data-bs-target="#githubModal"
+            >
+              En savoir plus
+            </button>
+            {/* Passe les données GitHub en props */}
+            <GithubModal githubData={githubData} />
           </div>
         </div>
       </div>
 
-      <div className="container my-5">
-        <div className="row">
-          <div className="col-md-6 mb-4">
-            <h3 className="HomeSubtitle2">A propos</h3>
-            <img
-              src="/images/john-doe-about.jpg"
-              alt="John Doe"
-              className="img-fluid mb-3"
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
-              necessitatibus consectetur tempore perferendis nostrum, ex
-              delectus reiciendis impedit aut iure enim placeat? Natus, neque
-              at?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
-              necessitatibus consectetur tempore perferendis nostrum, ex
-              delectus reiciendis impedit aut iure enim placeat? Natus, neque
-              at?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
-              necessitatibus consectetur tempore perferendis nostrum, ex
-              delectus reiciendis impedit aut iure enim placeat? Natus, neque
-              at?
-            </p>
-          </div>
-
-          <div className="col-md-6 mb-4">
-            <h3 className="HomeSubtitle2">Mes compétences</h3>
-
-            <p>HTML5 90%</p>
-            <div className="progress mb-3">
-              <div
-                className="progress-bar bg-danger"
-                style={{ width: "90%" }}
-              ></div>
-            </div>
-
-            <p>CSS3 80%</p>
-            <div className="progress mb-3">
-              <div
-                className="progress-bar bg-info"
-                style={{ width: "80%" }}
-              ></div>
-            </div>
-
-            <p>JAVASCRIPT 70%</p>
-            <div className="progress mb-3">
-              <div
-                className="progress-bar bg-warning"
-                style={{ width: "70%" }}
-              ></div>
-            </div>
-
-            <p>PHP 60%</p>
-            <div className="progress mb-3">
-              <div
-                className="progress-bar bg-success"
-                style={{ width: "60%" }}
-              ></div>
-            </div>
-
-            <p>REACT 50%</p>
-            <div className="progress mb-3">
-              <div
-                className="progress-bar bg-primary"
-                style={{ width: "50%" }}
-              ></div>
-            </div>
-          </div>
-        </div>
-      </div>
+      {/* ... le reste identique */}
     </div>
   );
 }
+
 export default Home;
