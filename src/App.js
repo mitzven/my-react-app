c45127a703fe608c59aca8b195b4a70197a42a37
Mileva <mileva3010@gmail.com>
2025-07-24 02:04:57 +0200

    first part page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import CustomNavbar from "./components/navbar";
-import Home from "./pages/Home";
+import Home from "./pages/Home.js";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,17 @@
+import "./Home.css";
+
 function Home() {
-  return <h1>Bienvenue sur la page d’accueil</h1>;
+  return (
+    <div className="home-container">
+      <div className="img-hero"> </div>
+      <div className="content">
+        <h1 className="HomeTitle">Bonjour, je suis John Doe</h1>
+        <h2 className="HomeSubtitle">Développeur web full stack</h2>
+        <div>
+          <button className="btn btn-danger">En savoir plus</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Home;
